314344c5ab9ccc96ff20ba7d00a6f8de016cf778
mrcolti4 <kbulygin456@gmail.com>
2025-03-15 13:43:47 +0200

    init project

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+  ],
+})
